a342d065eb66bb6637ae8f6225082428ee14d230
nolanj1 <john.nolan1@mycit.ie>
2019-05-11 21:51:30 +0100

    init

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM maven:3.6.0-jdk-8-slim AS build
 COPY src /home/app/src
 COPY pom.xml /home/app
 RUN mvn -f /home/app/pom.xml clean package
-
+EXPOSE 6568
 FROM java:8-jdk-alpine
 COPY --from=build /home/app/target/data-0.0.1-SNAPSHOT.jar /usr/local/lib/data.jar
 COPY --from=build /home/app/target/dependency-jars /usr/local/lib/dependency-jars
--- src/main/java/com/cit/micro/data/dao/LogDbDAO.java
+++ src/main/java/com/cit/micro/data/dao/LogDbDAO.java
@@ -91,7 +91,7 @@ public class LogDbDAO implements ILogDbDAO {
 
     @Override
     public LogData getChannel(String uid){
-        String sql = "SELECT uid, channel FROM stored_values WHERE uid=? AND channel <> '' LIMIT 0,1";
+        String sql = "SELECT channel FROM stored_values WHERE uid=? AND channel <> '' LIMIT 0,1";
         RowMapper<LogData> rowMapper = new DataRowMapper();
         return jdbcTemplate.queryForObject(sql, rowMapper, uid);
     }
