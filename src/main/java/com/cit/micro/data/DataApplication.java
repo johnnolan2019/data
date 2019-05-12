7c75cffc1ef8a6390c975bdccef3b929a1d8504d
nolanj1 <john.nolan1@mycit.ie>
2019-05-12 21:23:07 +0100

    init

--- src/main/java/com/cit/micro/data/DataApplication.java
+++ src/main/java/com/cit/micro/data/DataApplication.java
@@ -11,6 +11,5 @@ public class DataApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataApplication.class, args);
         ServerListener.serverRun();
-
     }
 }
