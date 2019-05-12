49ddfaf90cfdb2c2f2fb42875ecf3e763b750675
nolanj1 <john.nolan1@mycit.ie>
2019-05-12 13:52:31 +0100

    init

--- src/main/java/com/cit/micro/data/DataApplication.java
+++ src/main/java/com/cit/micro/data/DataApplication.java
@@ -3,12 +3,14 @@ package com.cit.micro.data;
 import com.cit.micro.data.service.ServerListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class DataApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataApplication.class, args);
         ServerListener.serverRun();
+
     }
 }
